7861b012d52fbda9297b33807a9402016fe9f8d9
SchmiedmeiszterAdam <smikuadam@gmail.com>
2022-03-23 11:44:15 +0100

    nyugta1 túllógás javítása

--- src/nyugtaprojektek/Nyugta1.java
+++ src/nyugtaprojektek/Nyugta1.java
@@ -3,9 +3,9 @@ package nyugtaprojektek;
 public class Nyugta1 {
 
     public static void main(String[] args) {
-        String csillag = "******************";
-        String vonal = "------------------";
-        String duplaVonal = "==================";
+        String csillag = "**********************";
+        String vonal = "----------------------";
+        String duplaVonal = "======================";
         String penzNem = "Ft";
         String rovidVonal = "_______";
         int tetel1 = 350;
@@ -16,24 +16,24 @@ public class Nyugta1 {
         double kedvezmenyesOsszeg = osszeg - (osszeg / kedvezemeny);
 
         System.out.println(csillag);
-        System.out.printf("%12s\n", "Nyugta1");
+        System.out.printf("%13s\n", "Nyugta1");
         System.out.println(csillag);
-        System.out.printf("%8s %5d %3s\n", "Tétel 1:", tetel1, penzNem);
-        System.out.printf("%8s %5d %3s\n", "Tétel 2:", tetel2, penzNem);
-        System.out.printf("%8s %5d %3s\n", "Tétel 3:", tetel3, penzNem);
+        System.out.printf("%9s %7d %3s\n", "Tétel 1:", tetel1, penzNem);
+        System.out.printf("%9s %7d %3s\n", "Tétel 2:", tetel2, penzNem);
+        System.out.printf("%9s %7d %3s\n", "Tétel 3:", tetel3, penzNem);
         System.out.println(duplaVonal);
-        System.out.printf("%9s %4s %3s\n", "Összesen:", osszeg, penzNem);
+        System.out.printf("%8s %7s %3s\n", "Összesen:", osszeg, penzNem);
         System.out.println(vonal);
-        System.out.printf("%5s %3s %3s\n", "Kedvezmény:", "109", penzNem);
+        System.out.printf("%6s %5s %3s\n", "Kedvezmény:", "109", penzNem);
         System.out.printf("(%d%s)\n", kedvezemeny, "%");
         System.out.println(duplaVonal);
         System.out.printf("%10s %.5g %3s\n", "Fizetendő:", kedvezmenyesOsszeg, penzNem);
         System.out.println(vonal);
         System.out.println("");
-        System.out.printf("%6s %9s\n", rovidVonal, rovidVonal);
-        System.out.printf("%6s %8s\n", "Dátum", "Név");
+        System.out.printf("%8s %11s\n", rovidVonal, rovidVonal);
+        System.out.printf("%7s %10s\n", "Dátum", "Név");
         System.out.println(csillag);
-        System.out.printf("%15s\n", "CÉG");
+        System.out.printf("%17s\n", "CÉG");
         System.out.println(csillag);
     }
 
